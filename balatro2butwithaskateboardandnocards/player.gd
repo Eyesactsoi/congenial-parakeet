1421423e28099db53e8a1be9424cebe8262ef34e
Eyesactsoi <162509564+Eyesactsoi@users.noreply.github.com>
2025-06-05 19:30:24 -0400

    Update player.gd

--- balatro2butwithaskateboardandnocards/player.gd
+++ balatro2butwithaskateboardandnocards/player.gd
@@ -1,4 +1,8 @@
 extends CharacterBody2D
+signal hit
+
+# Get the gravity from the project settings so you can sync with rigid body nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 #intializing our variables
 @export var speed = 400
@@ -8,4 +12,18 @@ var acceleration := 2.5
 var lastDirection: float
 
 const MAX_SPEED = 600.0
-const  JUMP_VELOCITY = -250.0
+const  jump_speed = -250.0
+
+func _physics_process(delta):
+	#adds gravity to the player
+	velocity.y += gravity * delta
+	
+	#if the player is going to jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+	
+	# Gets Directional Input
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	move_and_slide()
